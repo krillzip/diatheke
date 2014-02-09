5150843de1e38788986c02caeac61de753428a40
Kristoffer Paulsson <krillzip@crunchbang.(none)>
2014-02-09 17:33:59 +0100

    Some commands

---
+++ Command/DiathekeDiagnoseCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Krillzip\Diatheke\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Krillzip\Diatheke\Diatheke;
+
+/**
+ * Description of DiagnoseCommand
+ *
+ * @author krillzip
+ */
+class DiathekeDiagnoseCommand extends Command {
+
+    protected $output = null;
+    
+    protected function configure() {
+        $this
+                ->setName('diatheke:diagnose')
+                ->setDescription('Diagnosing the diatheke install')
+                ->setDefinition(array())
+                ->setHelp(<<<EOT
+The <info>diatheke:diagnose</info> command tests and diagnoses the Diatheke environment
+EOT
+                )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $d = new Diatheke();
+        $this->output = $output;
+        
+        $output->writeln('<info>Starting diagnosis...</info>');
+        
+        $output->writeln('<info>Probing OS for diatheke install</info>');
+        
+        if($d->isInstalled()){
+            $this->printSuccess('Diatheke environment installed');
+        }else{
+            $this->printError('Diatheke not installed');
+            return;
+        }
+    }
+    
+    protected function printSuccess($text){
+        $this->output->writeln('<fg=white;bg=green>'.$text.'</fg=white;bg=green>');
+    }
+    
+     protected function printError($text){
+        $this->output->writeln('<error>'.$text.'</error>');
+    }
+
+}
---
+++ Command/DiathekeListCommand.php
@@ -0,0 +1,152 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Krillzip\Diatheke\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use Krillzip\Diatheke\Diatheke;
+use Krillzip\Diatheke\OutputParser;
+use Krillzip\Diatheke\Configuration;
+
+/**
+ * Description of DiathekeListCommand
+ *
+ * @author krillzip
+ */
+class DiathekeListCommand extends Command {
+
+    protected function configure() {
+        $this
+                ->setName('diatheke:list')
+                ->setDescription('Lists installed diatheke modules')
+                ->setDefinition(array(
+                    new InputArgument('list', InputArgument::REQUIRED, '[all|bible|dictionary|commentary|book|locale]'),
+                    new InputOption('long', 'l', InputOption::VALUE_NONE, 'Long list (TODO)'),
+                    new InputOption('raw', 'r', InputOption::VALUE_NONE, 'Raw output'),
+                ))
+                ->setHelp(<<<EOT
+The <info>diatheke:list</info> command lists installed modules in the diatheke environment
+EOT
+                )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $d = new Diatheke();
+        $r = $input->getOption('raw');
+        $l = $input->getOption('long');
+
+        switch ($input->getArgument('list')) {
+            case 'all':
+                $data = $d->getModules();
+                if ($r) {
+                    $output->write($data, false, OutputInterface::OUTPUT_RAW);
+                } else {
+                    $structure = OutputParser::parseModules($data);
+                    $this->printStructure($structure, $output);
+                }
+                break;
+            case 'modules':
+                $data = $d->getModuleList();
+                if ($r) {
+                    $output->write($data, false, OutputInterface::OUTPUT_RAW);
+                } else {
+                    $structure = OutputParser::parseModuleList($data);
+                    $this->printListAsMatrix($structure, $output);
+                }
+                break;
+            case 'bible':
+            case 'bibles':
+                $data = $d->getModules();
+                 $structure = OutputParser::parseModules($data);
+                 $output->writeln('<comment>Biblical Texts:</comment>');
+                 $this->printList($structure['Biblical Texts'], $output);
+                break;
+            case 'dictionary':
+            case 'dictionaries':
+                $data = $d->getModules();
+                 $structure = OutputParser::parseModules($data);
+                 $output->writeln('<comment>Dictionaries:</comment>');
+                 $this->printList($structure['Dictionaries'], $output);
+                break;
+            case 'commentary':
+            case 'commentaries':
+                 $data = $d->getModules();
+                 $structure = OutputParser::parseModules($data);
+                 $output->writeln('<comment>Commentaries:</comment>');
+                 $this->printList($structure['Commentaries'], $output);
+                break;
+            case 'book':
+            case 'books':
+                $data = $d->getModules();
+                $structure = OutputParser::parseModules($data);
+                $output->writeln('<comment>Generic books:</comment>');
+                $this->printList($structure['Generic books'], $output);
+                break;
+            case 'locale':
+            case 'locales':
+                $data = $d->getLocales();
+                if ($r) {
+                    $output->write($data, false, OutputInterface::OUTPUT_RAW);
+                } else {
+                    $structure = OutputParser::parseModuleList($data);
+                    $this->printListAsMatrix($structure, $output);
+                }
+                break;
+        }
+    }
+
+    protected function getLongestName(array $names) {
+        if (!empty($names)) {
+            return max(array_map('strlen', $names)) + 1;
+        } else {
+            return 0;
+        }
+    }
+
+    protected function printStructure(array $structure, OutputInterface $output) {
+        foreach ($structure as $name => $section) {
+            $output->writeln('<comment>' . $name . ':</comment>');
+            if(!empty($section)){
+                $this->printList($section, $output);
+            }else{
+                $output->writeln('   <error>No entries</error>');
+            }
+        }
+    }
+
+    protected function printList(array $list, OutputInterface $output) {
+        $longest = $this->getLongestName(array_keys($list));
+        foreach ($list as $abbr => $description) {
+            $output->writeln('    <info>' . $abbr . '</info>' . str_pad(' ', $longest - strlen($abbr)) . '- ' . $description);
+        }
+    }
+    
+    protected function printListAsMatrix(array $list, OutputInterface $output) {
+        $longest = $this->getLongestName($list);
+        $dim = $this->getApplication()->getTerminalDimensions();
+        $rowMax = (int)($dim[0] / $longest);
+        
+        if($rowMax < 1){
+            $rowMax = 1;
+        }
+        
+        while(count($list) > 0){
+            $row = '';
+            for($i = 0; $i < $rowMax; $i++){
+                $abbr = array_shift($list);
+                $row .= $abbr.str_pad(' ', $longest - strlen($abbr));
+            }
+            $output->writeln($row);
+        }
+    }
+}
